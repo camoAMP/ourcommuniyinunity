85ea38b253133b5ba8a90438926b185f0812daee
cameron <cameron@chicagoamp.com>
2026-02-03 22:08:56 +0200

    Ensure OpenNext uses next build

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 
-export default defineCloudflareConfig({
+const baseConfig = defineCloudflareConfig();
+
+export default {
+  ...baseConfig,
   buildCommand: "next build",
-});
+};
